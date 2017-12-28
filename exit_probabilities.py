dd4367fafc512e7c56f3915ee1ab12bc6913a173
vbehnam <33384279+vbehnam@users.noreply.github.com>
2017-12-27 22:51:35 -0500

    Update exit_probabilities.py

--- exit_probabilities.py
+++ exit_probabilities.py
@@ -4,49 +4,49 @@ import numpy.matlib
 def main():
     p = [1/4, 2/4, 1/4]
     dist = [1, 1]
-    exit = [0, 100]
+    exitp = [0, 100]
     t = 100
-    exit_prob(p, dist, exit, t)
+    exit_prob(p, dist, exitp, t)
 
 # parameters: p is a vector of three values, each signifying the probability of moving left, right and staying in the same spot respectively
 # dist is a vector of two values, signifying the distance that each particle moves left and right respectively. make sure the order matches p!!!
-# exit is vector of two values, signifying where the particle stops moving (or exits). The lower value should be exit[1]
-# t is the exit you are interested in. It must be equal to one of the two values in exit
+# exitp is vector of two values, signifying where the particle stops moving (or exits). The lower value should be exitp[0]
+# t is the exit you are interested in. It must be equal to one of the two values in exitp
 
 # output: a vector of the probabilities of exiting, with each -ith element signifying the probability of hitting t (the fourth argument),
 # given that you have started from the ith position
 # i.e. U[80] is the probability of hitting t, given that your initial position was 80
 
-def exit_prob(p, dist, exit, t):
+def exit_prob(p, dist, exitp, t):
 
     if sum(p) != 1:
         raise ProbabilityError("p does not sum to 1!")
 
-    if len(p) != len(dist) + 1 or len(dist) != len(exit):
+    if len(p) != len(dist) + 1 or len(dist) != len(exitp):
         raise LengthMismatch("the lengths of the vectors do not match up!")
 
-    if type(dist[0]) != int or type(dist[1]) != int or type(exit[0]) != int or type(exit[1]) != int:
+    if type(dist[0]) != int or type(dist[1]) != int or type(exitp[0]) != int or type(exitp[1]) != int:
         raise NotInteger("one or more of dist entries is not an integer")
 
-    if t != exit[0] and t != exit[1]:
+    if t != exitp[0] and t != exitp[1]:
         raise ExitMismatch("neither of the exit values are equal to t!")
 
     # the smaller number should be first. In the event that it is not, they will be switched
-    if exit[0] > exit[1]:
-        temp = exit[1]
-        exit[1] = exit[0]
-        exit[0] = temp
+    if exitp[0] > exitp[1]:
+        temp = exitp[1]
+        exitp[1] = exitp[0]
+        exitp[0] = temp
 
-    # if exit[0] == exit[1] then we are already trivially at the boundary
-    if exit[0] == exit[1]:
+    # if exitp[0] == exitp[1] then we are already trivially at the boundary
+    if exitp[0] == exitp[1]:
         return 0
 
 
     # the possible moves the particle can take are only the points between the two exits
-    n = exit[1] - exit[0]
+    n = exitp[1] - exitp[0]
 
     # however, this is not the dimension yet, as it is possible that, if the possible distance moved is greater than 1,
-    # that the particle "jumps" over exit[0] into "exit[-1]". hence, we need to account for all the possible moves it can take
+    # that the particle "jumps" over exitp[0] into "exitp[-1]". hence, we need to account for all the possible moves it can take
     n = n + dist[0] - 1
 
     # same goes for the other side (i.e. dist[1])
@@ -65,11 +65,11 @@ def exit_prob(p, dist, exit, t):
 
 
     # b should contain one on the exits on the side of t! (not both exits on both extremes, hence the if statements!)
-    if exit[0] == t:
+    if exitp[0] == t:
         for i in range(0, dist[0]):
             b[i] = 1
 
-    elif exit[1] == t:
+    elif exitp[1] == t:
         for i in range(n-1, n-dist[1]-1, -1):
             b[i] = 1
 
